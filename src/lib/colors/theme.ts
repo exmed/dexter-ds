287d8be3bedc64008789c31258e9e0789a7b8337
Gabriel Lourenço <gaabriiel.xd@gmail.com>
2023-08-21 09:53:27 -0300

    criação de arquivos base

---
+++ src/lib/colors/theme.ts
@@ -0,0 +1,79 @@
+const colors = {
+	brand0: '#00D1BC',
+
+	// Action
+	actionDefault: '#B1EFE8',
+	actionFocus: '#89E9DE',
+	actionHover: '#99EAE1',
+	actionPressed: '#26BAA9',
+	actionDisabled: '#D0F5F1',
+
+	// Empresas
+	empresasDefault: '#6161FF',
+	empresasFocus: '#5D5DF6',
+	empresasHover: '#5858ED',
+	empresasPressed: '#4646C2',
+	empresasDisabled: '#D3D3FB',
+
+	// Neutral
+	neutralLight00: '#FFFFFF',
+	neutralLight10: '#FAFAFA',
+	neutralLight20: '#F7F7F7',
+	neutralMid30: '#E2E1E2',
+	neutralMid40: '#CCCBCD',
+	neutralMid50: '#8A898E',
+	neutralMid60: '#747379',
+	neutralDark70: '#545356',
+	neutralDark80: '#333333',
+	neutralDark90: '#141414',
+
+	// Feedback
+	feedbackPositive: '#107048',
+	feedbackNegative: '#BE3E37',
+	feedbackWarning: '#EFBA2A',
+	feedbackInfo: '#23489F',
+
+	// Blue
+	blueDefault: '#2684B5',
+	blueFocus: '#4E9AC2',
+	blueHover: '#3A8FBC',
+	bluePressed: '#2277A4',
+	blueDisabled: '#C5DCE8',
+
+	// Yellow
+	yellowDefault: '#FFE999',
+	yellowFocus: '#F9E086',
+	yellowHover: '#FCE590',
+	yellowPressed: '#F2D773',
+	yellowDisabled: '#FBF2D3',
+
+	// Red
+	redDefault: '#D46164',
+	redFocus: '#C1585B',
+	redHover: '#DA6F72',
+	redPressed: '#DF7D7F',
+	redDisabled: '#FFCFD0',
+
+	// Lilac
+	lilacDefault: '#8E95E6',
+	lilacFocus: '#A5ABEB',
+	lilacHover: '#888EDC',
+	lilacPressed: '#8187D1',
+	lilacDisabled: '#E8EAFA',
+
+	// Orange
+	orangeDefault: '#FF8D12',
+	orangeFocus: '#FF9727',
+	orangeHover: '#FFA13B',
+	orangePressed: '#E88010',
+	orangeDisabled: '#FFDCB5',
+
+	//Green
+	greenDefault: '#26BAA9',
+	greenFocus: '#28C3B1',
+	greenHover: '#2ACBB8',
+	greenPressed: '#1F988A',
+	greenDisabled: '#A4DFDA',
+};
+
+export default colors;
